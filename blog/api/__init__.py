30fd72d724e3a3e9188bed9c4c6616abb13846e6
iLn0ur <you@example.com>
2023-03-27 14:10:27 +0300

    l10

--- blog/api/__init__.py
+++ blog/api/__init__.py
@@ -1,12 +1,15 @@
 from flask_combo_jsonapi import Api
 from blog.api.tag import TagList, TagDetail
 from combojsonapi.spec import ApiSpecPlugin
+from combojsonapi.event import EventPlugin
 from blog.api.user import UserList, UserDetail
 from blog.api.author import AuthorList, AuthorDetail
 from blog.api.article import ArticleList, ArticleDetail
+from combojsonapi.permission import PermissionPlugin
 
 
 def create_api_spec_plugin(app):
+
     api_spec_plugin = ApiSpecPlugin(
         app=app,
         # Declaring tags list with their descriptions,
@@ -22,11 +25,15 @@ def create_api_spec_plugin(app):
 
 
 def init_api(app):
+    event_plugin = EventPlugin()
     api_spec_plugin = create_api_spec_plugin(app)
+    permission_plugin = PermissionPlugin(strict=False)
     api = Api(
         app,
         plugins=[
+            event_plugin,
             api_spec_plugin,
+            permission_plugin,
         ],
     )
     api.route(TagList, "tag_list", "/api/tags/", tag="Tag")
